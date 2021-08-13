d491cbfcd7e92cd18a413520aeae8353316c05e6
codeinsack <codeinsack@gmail.com>
2021-08-13 23:09:50 +0300

    add get random word endpoint

--- loopback/common/models/word.js
+++ loopback/common/models/word.js
@@ -1,5 +1,29 @@
 "use strict";
 
+const app = require("../../server/server");
+
+const dbConnect = () =>
+  new Promise((resolve, reject) => {
+    const model = app.models.Word;
+    model.getDataSource().connector.connect((err, db) => {
+      if (err) return reject(err);
+      resolve(db);
+    });
+  });
+
 module.exports = function (Word) {
   Word.validatesUniquenessOf("word");
+
+  Word.getRandomWord = async () => {
+    const db = await dbConnect();
+    const collection = db.collection("Word");
+    const pipeline = [{ $sample: { size: 1 } }];
+    const pupils = await collection.aggregate(pipeline);
+    return pupils.toArray();
+  };
+
+  Word.remoteMethod("getRandomWord", {
+    returns: { arg: "data" },
+    http: { path: "/random", verb: "get" },
+  });
 };
